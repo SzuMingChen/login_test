6fc92401e91fcb973fce98cb359dcbe3ccce9c1d
SzuMingChen <kyo7832222@gmail.com>
2023-11-24 16:38:00 +0800

    新增資料庫建立語法

--- database/init.sql
+++ database/init.sql
@@ -1,4 +1,4 @@
--- Active: 1700125805233@@192.168.85.101@3306@todo_list
+-- Active: 1700814283183@@192.168.86.182@3306@todo_list
 CREATE TABLE `user_account_table` (
    `user_account` VARCHAR(255) NOT NULL COMMENT '帳號',
    `password` VARCHAR(255) DEFAULT '' COMMENT '密碼',
@@ -6,7 +6,9 @@ CREATE TABLE `user_account_table` (
    `status` TINYINT NOT NULL DEFAULT '0' COMMENT '帳號狀態, 0:關閉;  1:創建',
    `account_update_time` DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT '建立時間'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='帳號資訊';
-
+ALTER TABLE `todo_list`.`user_account_table` 
+ADD PRIMARY KEY (`user_account`);
+;
 CREATE TABLE `todo_list_table` (
    `id` INT NOT NULL AUTO_INCREMENT COMMENT '流水id',
    `user_account` VARCHAR(255) NOT NULL COMMENT '帳號',
